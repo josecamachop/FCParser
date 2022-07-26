d663c9050c1dc14d3e36de74d9465564040618da
José Camacho <josecamacho@ugr.es>
2022-07-26 13:40:20 +0200

    Update fclearner.py

--- bin/fclearner.py
+++ bin/fclearner.py
@@ -463,13 +463,13 @@ def filter_instances(instances, perc):
     threshold = perc*instances['count']
     for varkey in instances.keys():
         if varkey != 'count':
-            delete = {}
+            delete = []
     
             for feakey in instances[varkey].keys():
                 if instances[varkey][feakey] < threshold:
                     delete.append(feakey)
                     
-            for feakey in delete.keys():
+            for feakey in delete:
                 del instances[varkey][feakey]
                 if len(instances[varkey].keys()) == 0:
                     del instances[varkey] 
